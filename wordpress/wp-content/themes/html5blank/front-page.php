7a560d78539b8ac646816930692c258a159cea7f
Constantino Schillebeeckx <titous@gmail.com>
2016-07-15 15:17:05 -0700

    tmp

---
+++ wordpress/wp-content/themes/html5blank/front-page.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+
+    <main role="main">
+    <!-- section -->
+    <section>
+
+
+    <!-- article -->
+    <article id="search">
+        <?php // content here 
+            global $current_user;
+            $db = new Database( get_user_meta( $current_user->ID, 'company', true) );
+            $db->show();
+        ?>
+
+    </article>
+    <!-- /article -->
+
+    </section>
+    <!-- /section -->
+    </main>
+
+<?php //get_sidebar(); ?>
+
+<?php get_footer(); ?>
+
