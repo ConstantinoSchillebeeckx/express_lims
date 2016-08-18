7f2be0f616e4b9e860be968c436b71638e48af1b
Constantino Schillebeeckx <titous@gmail.com>
2016-08-17 17:43:31 -0700

    delete table implemented

--- wordpress/wp-content/themes/html5blank/add_table.php
+++ wordpress/wp-content/themes/html5blank/add_table.php
@@ -12,12 +12,16 @@ get_header(); ?>
 
             <?php if ( is_user_logged_in() ) { ?>
 
-                <h1><?php the_title(); ?></h1>
+                <?php if (isset($_GET['table'])) {
+                    echo "<h1>Edit table <code>" . $_GET['table'] . "</code></h1>";
+                } else {
+                    echo '<h1>' . the_title() . '</h1>';
+                }?>
 
                 <!-- article -->
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-                    <form class="form-horizontal" onsubmit="addTable()">
+                    <form class="form-horizontal">
                       <div class="form-group">
                         <label for="inputEmail3" class="col-sm-2 control-label">Table name</label>
                         <div class="col-sm-2">
@@ -27,7 +31,12 @@ get_header(); ?>
                           <button type="button" class="btn btn-default btn-info" onclick="addField()" id="add_field" >Add field</button>
                         </div>
                         <div class="col-sm-offset-4 col-sm-2">
-                          <button type="submit" class="btn btn-default pull-right btn-success">Create table</button>
+                          <?php if (isset($_GET['table'])) {
+                            echo '<div class="pull-right btn-group"><button type="submit" class="btn btn-success" onclick="editTable()">Save table</button>';
+                            echo '<button class="btn btn-danger"><i class="fa fa-times" aria-hidden="true" onclick="deleteTable(\'' . $_GET['table'] . '\')"></i></button></div>';
+                          } else {
+                            echo '<button class="btn pull-right btn-success" onclick="addTable()">Create table</button>';
+                          }?>
                         </div>
                       </div>
                     </form>
